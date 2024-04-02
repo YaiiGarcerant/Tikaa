32d77e2e8374555c0bdecc7100817a14c1a05547
Yaine Garcerant <yainegarcerant964@gmail.com>
2024-04-02 08:31:45 -0500

    Agregando Archivos

---
+++ asset/js/app.js
@@ -0,0 +1,56 @@
+window.addEventListener('load', function() {
+    setTimeout(function() {
+        document.getElementById('preloader').style.display = 'none';
+        document.querySelector('.content').classList.remove('hidden');
+        var elements = document.querySelectorAll('.fade-in');
+        elements.forEach(function(element) {
+            element.classList.add('fade-in-active');
+        });
+    }, 3000);
+});
+//
+let form = document.querySelector('.form-register');
+let progressOptions = document.querySelectorAll('.progressbar__option');
+
+form.addEventListener('click', function(e) {
+    let element = e.target;
+    let isButtonNext = element.classList.contains('step__button--next');
+    let isButtonBack = element.classList.contains('step__button--back');
+    if (isButtonNext || isButtonBack) {
+        let currentStep = document.getElementById('step-' + element.dataset.step);
+        let jumpStep = document.getElementById('step-' + element.dataset.to_step);
+        currentStep.addEventListener('animationend', function callback() {
+            currentStep.classList.remove('active');
+            currentStep.classList.add('d-none');
+            jumpStep.classList.add('active');
+            jumpStep.classList.remove('d-none');
+            if (isButtonNext) {
+                currentStep.classList.add('to-left');
+                progressOptions[element.dataset.to_step - 1].classList.add('active');
+            } else {
+                jumpStep.classList.remove('to-left');
+                progressOptions[element.dataset.step - 1].classList.remove('active');
+            }
+            currentStep.removeEventListener('animationend', callback);
+        });
+        currentStep.classList.add('inactive');
+        jumpStep.classList.remove('inactive');
+    }
+});
+//
+
+
+
+
+//
+// window.addEventListener('load', function() {
+//     setTimeout(function() {
+//         document.getElementById('preloader').style.display = 'none';
+//         document.querySelector('.content').classList.remove('hidden');
+//         var elements = document.querySelectorAll('.fade-in');
+//         elements.forEach(function(element) {
+//             element.classList.add('fade-in-active');
+//         });
+//     }, 3000);
+// });
+//
