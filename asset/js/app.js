f2d7f2d8b3d0d0ba624097448a23f50a3cb5396d
Yaine Garcerant <yainegarcerant964@gmail.com>
2024-04-03 14:02:08 -0500

    Arreglo Wizard

--- asset/js/app.js
+++ asset/js/app.js
@@ -1,9 +1,9 @@
-window.addEventListener('load', function() {
-    setTimeout(function() {
+window.addEventListener('load', function () {
+    setTimeout(function () {
         document.getElementById('preloader').style.display = 'none';
         document.querySelector('.content').classList.remove('hidden');
         var elements = document.querySelectorAll('.fade-in');
-        elements.forEach(function(element) {
+        elements.forEach(function (element) {
             element.classList.add('fade-in-active');
         });
     }, 3000);
@@ -11,47 +11,31 @@ window.addEventListener('load', function() {
 
 let form = document.querySelector('#form-register');
 let progressOptions = document.querySelectorAll('.progressbar__option');
-let transitioning = false;
 
-form.addEventListener('click', function (e) {
+form.addEventListener('click', function(e) {
     let element = e.target;
     let isButtonNext = element.classList.contains('step__button--next');
     let isButtonBack = element.classList.contains('step__button--back');
     if (isButtonNext || isButtonBack) {
-        let currentStep = document.querySelector('.step.active');
-        let nextStepId = isButtonNext ? parseInt(currentStep.id.split('-')[1]) + 1 : parseInt(currentStep.id.split('-')[1]) - 1;
-        let nextStep = document.getElementById('step-' + nextStepId);
-
-        if (transitioning) {
-            return; // Si ya hay una transición en curso, no hacer nada
-        }
-
-        transitioning = true; // Indicar que hay una transición en curso
-
-        currentStep.addEventListener('transitionend', function() {
+        let currentStep = document.getElementById('step-' + element.dataset.step);
+        let jumpStep = document.getElementById('step-' + element.dataset.to_step);
+        currentStep.addEventListener('animationend', function callback() {
+            currentStep.classList.remove('active');
             currentStep.classList.add('d-none');
-            nextStep.classList.remove('slide-in-right');
-            transitioning = false; // Restablecer el estado después de la transición
+            jumpStep.classList.add('active');
+            jumpStep.classList.remove('d-none');
+            if (isButtonNext) {
+                currentStep.classList.add('to-left');
+                progressOptions[element.dataset.to_step - 1].classList.add('active');
+            } else {
+                jumpStep.classList.remove('to-left');
+                progressOptions[element.dataset.step - 1].classList.remove('active');
+            }
+            currentStep.removeEventListener('animationend', callback);
         });
-
-        currentStep.classList.remove('active');
-        currentStep.classList.add('slide-out-left');
         currentStep.classList.add('inactive');
-
-        nextStep.classList.remove('slide-out-left');
-        nextStep.classList.remove('inactive');
-        nextStep.classList.add('active');
-        nextStep.classList.add('slide-in-right');
-        nextStep.classList.remove('d-none'); // Mostrar el siguiente paso
-
-        progressOptions[nextStepId - 1].classList.add('active');
-
-        if (isButtonNext) {
-            currentStep.classList.add('to-left');
-        } else {
-            currentStep.classList.remove('to-left');
-            progressOptions[nextStepId].classList.remove('active');
-        }
+        jumpStep.classList.remove('inactive');
     }
 });
+//
 
