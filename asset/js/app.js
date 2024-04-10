7f62215d351d901932e7ae80dc8ba66d20785fe4
YaiiGarcerant <126992301+YaiiGarcerant@users.noreply.github.com>
2024-04-10 05:07:57 +0000

    Boton Reset

--- asset/js/app.js
+++ asset/js/app.js
@@ -1,13 +1,3 @@
-window.addEventListener('load', function () {
-    setTimeout(function () {
-        document.getElementById('preloader').style.display = 'none';
-        document.querySelector('.content').classList.remove('hidden');
-        var elements = document.querySelectorAll('.fade-in');
-        elements.forEach(function (element) {
-            element.classList.add('fade-in-active');
-        });
-    }, 3000);
-});
 
 let form = document.querySelector('#form-register');
 let progressOptions = document.querySelectorAll('.progressbar__option');
